918e97694ba33aaa118cc78e5643d5d1b597c662
Peter Vu <vutuananh.vn.sg@gmail.com>
2021-09-02 17:32:42 +0800

    Test

---
+++ module1/src/main/java/org/example/module1/Module1.java
@@ -0,0 +1,7 @@
+package org.example.module1;
+
+public class Module1 {
+    public static void main(String... args) {
+        System.out.println("Module 1");
+    }
+}
---
+++ module2/src/main/java/org/example/module2/Module2.java
@@ -0,0 +1,7 @@
+package org.example.module2;
+
+public class Module2 {
+    public static void main(String ...args) {
+        System.out.println("Module 2");
+    }
+}
